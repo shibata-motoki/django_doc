106283f1577de07233bd3d149b35292335b17861
shibata-motoki <motoki.shibata@nex.nikkei.com>
2018-08-16 17:19:05 +0900

    tutorial7

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,17 +3,41 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
+
+"""
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    """
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    """
     return render(request, 'polls/detail.html', {'question': question})
 #    return HttpResponse("You're looking at question %s." % question_id)
 
@@ -43,3 +67,4 @@ def index(request):
     }
     return render(request, 'polls/index.html', context)
 #    return HttpResponse(template.render(context, request))
+"""
